4cdb36ddab20da555a18e267c5eb487cc45e4183
Manuel Barroso Pecellín <manubp1997@gmail.com>
2026-01-16 12:12:40 +0100

    Funcionalidad mostrar productos para el administrador

--- src/main/java/com/manubp/ecommerce/service/ProductoService.java
+++ src/main/java/com/manubp/ecommerce/service/ProductoService.java
@@ -1,5 +1,6 @@
 package com.manubp.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.manubp.ecommerce.model.Producto;
@@ -13,5 +14,8 @@ public interface ProductoService {
 	public void update(Producto producto);
 	
 	public void delete(Integer id);
+	
+	// Listar todos los productos
+	public List<Producto> findAll();
 
 }
