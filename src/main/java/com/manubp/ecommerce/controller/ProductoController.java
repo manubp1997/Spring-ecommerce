81de6c6194e5d16ad2e544bd726b134bf143aca6
Manuel Barroso Pecellín <manubp1997@gmail.com>
2026-01-16 11:57:27 +0100

    Modificar templates, desarrollo de funcionalidades guardado productos y creación de Logger

--- src/main/java/com/manubp/ecommerce/controller/ProductoController.java
+++ src/main/java/com/manubp/ecommerce/controller/ProductoController.java
@@ -1,18 +1,48 @@
 package com.manubp.ecommerce.controller;
 
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.manubp.ecommerce.model.Producto;
+import com.manubp.ecommerce.model.Usuario;
+import com.manubp.ecommerce.service.ProductoService;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
+	
+	// Esto es para registrar logs en la consola
+	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+	
+	@Autowired
+	private ProductoService productoService;
 	
 	@GetMapping("")
 	public String show() {
 		return "productos/show";
 	}
 	
+	@GetMapping("/create")
+	public String create() {
+		return "productos/create";
+	}
+	
+	
+	@PostMapping("/save")
+	public String save(Producto producto) {
+		
+		LOGGER.info("Este es el producto guardado: {}", producto);		
+		Usuario u = new Usuario(1, "", "", "", "", "", "", "");
+		producto.setUsuario(u);
+		
+		productoService.save(producto);
+		
+		return "redirect:/productos";
+	}
 	
 
 }
