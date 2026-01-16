7340e23a9d2c4ca5963ebf1a150d14a50a2e2230
Manuel Barroso Pecellín <manubp1997@gmail.com>
2026-01-16 01:39:29 +0100

    Crear repository y las clases del servicio. También el controlador de producto

---
+++ src/main/java/com/manubp/ecommerce/service/ProductoService.java
@@ -0,0 +1,17 @@
+package com.manubp.ecommerce.service;
+
+import java.util.Optional;
+
+import com.manubp.ecommerce.model.Producto;
+
+public interface ProductoService {
+	
+	public Producto save(Producto producto);
+	
+	public Optional<Producto> get(Integer id);
+	
+	public void update(Producto producto);
+	
+	public void delete(Integer id);
+
+}
