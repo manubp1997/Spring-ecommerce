c42e1272c8a9c86ee46b27a98262340405fd6701
Manuel Barroso Pecellín <manubp1997@gmail.com>
2026-01-19 12:32:04 +0100

    Mostrar vista del producto

--- src/main/java/com/manubp/ecommerce/controller/HomeController.java
+++ src/main/java/com/manubp/ecommerce/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.manubp.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.manubp.ecommerce.service.ProductoService;
@@ -15,6 +18,7 @@ public class HomeController {
 	@Autowired
 	private ProductoService productoService;
 	
+	private final Logger log = LoggerFactory.getLogger(HomeController.class);
 	
 	@GetMapping("")
 	public String home(Model model) {
@@ -23,5 +27,13 @@ public class HomeController {
 		
 		return "usuario/home";
 	}	
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id) {
+
+		log.info("Id producto enviado como parametro {}", id);
+		
+		return "usuario/productohome";
+	}
 
 }
