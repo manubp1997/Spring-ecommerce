546ac008b60f1298ebc2f54f0cb32c23e2710dce
Manuel Barroso Pecellín <manubp1997@gmail.com>
2026-01-19 13:43:19 +0100

    Mostrar los datos de los productos en la vista productohome

--- src/main/java/com/manubp/ecommerce/controller/HomeController.java
+++ src/main/java/com/manubp/ecommerce/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.manubp.ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.manubp.ecommerce.model.Producto;
 import com.manubp.ecommerce.service.ProductoService;
 
 @Controller
@@ -29,9 +32,16 @@ public class HomeController {
 	}	
 	
 	@GetMapping("productohome/{id}")
-	public String productoHome(@PathVariable Integer id) {
+	public String productoHome(@PathVariable Integer id, Model model) {
 
 		log.info("Id producto enviado como parametro {}", id);
+		
+		Producto producto = new Producto();
+		Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get();
+		
+		
+		model.addAttribute("producto", producto);
 		
 		return "usuario/productohome";
 	}
