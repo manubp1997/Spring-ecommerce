ad8666db0b59469632030dbb7e0cf6001cc3618f
Manuel Barroso Pecellín <manubp1997@gmail.com>
2026-01-20 10:29:39 +0100

    Añadir productos al carrito indicando los datos y la suma total

--- src/main/java/com/manubp/ecommerce/controller/HomeController.java
+++ src/main/java/com/manubp/ecommerce/controller/HomeController.java
@@ -59,7 +59,7 @@ public class HomeController {
 	}
 	
 	@PostMapping("/cart")
-	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad) {
+	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad, Model model) {
 		
 		DetalleOrden detalleOrden = new DetalleOrden();
 		Producto producto = new Producto();
@@ -70,6 +70,21 @@ public class HomeController {
 		
 		log.info("Producto agregado: {}", optionalProducto.get());
 		log.info("Cantidad: {}", cantidad);
+		producto = optionalProducto.get();
+		
+		detalleOrden.setCantidad(cantidad);
+		detalleOrden.setPrecio(producto.getPrecio());
+		detalleOrden.setNombre(producto.getNombre());
+		detalleOrden.setTotal(producto.getPrecio() * cantidad);
+		detalleOrden.setProducto(producto);
+		
+		detalles.add(detalleOrden);
+		
+		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
+		orden.setTotal(sumaTotal);
+		
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
 		
 		return "usuario/carrito";
 	}
