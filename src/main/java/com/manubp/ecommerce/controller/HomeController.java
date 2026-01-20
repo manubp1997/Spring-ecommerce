7373cb0927b96fae4d196ae9eed748fde9613512
Manuel Barroso Pecellín <manubp1997@gmail.com>
2026-01-20 11:52:06 +0100

    Añadimos la funcionalidad para ver el carrito y validación de duplicados de productos

--- src/main/java/com/manubp/ecommerce/controller/HomeController.java
+++ src/main/java/com/manubp/ecommerce/controller/HomeController.java
@@ -78,7 +78,14 @@ public class HomeController {
 		detalleOrden.setTotal(producto.getPrecio() * cantidad);
 		detalleOrden.setProducto(producto);
 		
-		detalles.add(detalleOrden);
+		//verificar que el producto no se añada dos veces
+		Integer idProducto = producto.getId();
+		boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getId() == idProducto);
+		
+		if(!ingresado) {
+			detalles.add(detalleOrden);
+		}
+		
 		
 		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
 		orden.setTotal(sumaTotal);
@@ -116,6 +123,15 @@ public class HomeController {
 		
 		return "usuario/carrito";
 		
+	}
+	
+	@GetMapping("/getCart")
+	public String getCart(Model model) {
+		
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		
+		return "usuario/carrito";
 	}
 
 }
