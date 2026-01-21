93967e2acdf1ffb1245a5811490cdd41d2d99624
Manuel Barroso Pecellín <manubp1997@gmail.com>
2026-01-21 14:28:07 +0100

    Creación de la funcionalidad para el registro de usuarios

--- src/main/java/com/manubp/ecommerce/impl/UsuarioServiceImpl.java
+++ src/main/java/com/manubp/ecommerce/impl/UsuarioServiceImpl.java
@@ -20,6 +20,12 @@ public class UsuarioServiceImpl implements IUsuarioService {
 		return usuarioRepository.findById(id);
 	}
 
+	@Override
+	public Usuario save(Usuario usuario) {
+		
+		return usuarioRepository.save(usuario);
+	}
+
 
 
 }
--- src/main/java/com/manubp/ecommerce/service/IUsuarioService.java
+++ src/main/java/com/manubp/ecommerce/service/IUsuarioService.java
@@ -7,5 +7,7 @@ import com.manubp.ecommerce.model.Usuario;
 public interface IUsuarioService {
 	
 	Optional<Usuario> findById(Integer id);
+	
+	Usuario save(Usuario usuario);
 
 }
